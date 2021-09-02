7810919a4fc449bec77b36715d5e66f2914f146e
tsweezy <sweettre216@gmail.com>
2021-09-02 19:31:55 -0400

    Add new (in progress) version of website
    Culmination of months of on/off work in trying to find a more intuitive and visually pleasing design.

---
+++ new/script.js
@@ -0,0 +1,12 @@
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+
+window.addEventListener("scroll", function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").style.top = "1em";
+    } else {
+        document.getElementById("header").style.top = "-10em";
+    }
+    prevScrollpos = currentScrollPos;
+})
